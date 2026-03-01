a9f4e6ad352e1eaff1e31061bdca594de43b5bb1
HP_ece\Ece <if23b062@technikum-wien.at>
2026-03-01 17:12:37 +0100

    change in implementation logic

--- main.py
+++
@@ -1,5 +0,0 @@
-from src.schema_extractor import extract_schema
-
-northwind_schema = extract_schema("Databases/sakila.db")
-print("NORTHWIND SCHEMA:")
-print(northwind_schema[:500])  # print first 500 chars
